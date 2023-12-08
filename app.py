b34ef9d4743dd129ff3a4815321fa836e90a0c94
Tyrone <tyronemguni@gmail.com>
2023-12-08 19:06:05 +0200

    Cache fallback message in response

--- app.py
+++ app.py
@@ -21,10 +21,12 @@ async def social_network_activity():
     time_sensitive = request.args.get('time_sensitive', "True")
     if time_sensitive.lower() == "false":
         cached_data = cache.get("social_media_activity")
+        cached_data["message"] = {}
         if cached_data is not None : return jsonify(cached_data)
 
     data_from_all_social_network = await asyncio.gather(*[get_social_network_data(url["url"], url["name"]) for url in urls])
     json_response = {}
+    message = {}
 
     for index, data in enumerate(data_from_all_social_network):
         if data is not None:
@@ -32,8 +34,13 @@ async def social_network_activity():
         else:
             cached_data = cache.get("social_media_activity")
             json_response[urls[index]["name"]] = cached_data.get(urls[index]["name"]) if cached_data is not None else 0
+            cached_data_fallback_message = "Eratic response from server, falling back to last known cached value."
+            failed_cache_fallback_message = "Got an eratic response from server, but cache is empty. Defaulting to 0 activity for now."
+            message[urls[index]["name"]] = cached_data_fallback_message if cached_data is not None else failed_cache_fallback_message
 
     cache.set("social_media_activity", json_response, timeout=300)
+
+    json_response["message"] = message
 
     return json_response
 
